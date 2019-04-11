ccecd1420f113b0e4a699e13300fbc812783eb1a
navnry <1977234700@qq.com>
2019-04-11 10:54:06 +0800

    1

--- js/common.js
+++ js/common.js
@@ -2,7 +2,21 @@ $(".menu").click(function () {
     $(".nav").toggleClass("on");
     event.preventDefault()
 });
-$(".nav > ul > li").click(function () {
+$(".nav > ul > li").click(function (e) {
     $(this).addClass("on").children(".submenu").stop().slideToggle(300);
     $(this).siblings().removeClass("on").children(".submenu").slideUp(300)
+    e.stopPropagation()
+});
+$(".nav").on("touchmove", function (e) {
+    $(this).removeClass("on")
+    e.stopPropagation()
+});
+$(".nav>ul").on("touchmove", function (e) {
+    e.stopPropagation()
+});
+$(".nav>ul").on("click", function (e) {
+    e.stopPropagation()
+});
+$(".nav").on("click", function (e) {
+    $(this).removeClass("on")
 });
